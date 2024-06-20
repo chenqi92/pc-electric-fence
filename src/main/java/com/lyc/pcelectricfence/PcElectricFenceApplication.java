18739c8e910cac6ae884d705ddd83d977e2491d0
chenqi <chenqi92104@icloud.com>
2024-06-20 11:45:24 +0800

    init 项目初始化提交

---
+++ src/main/java/com/lyc/pcelectricfence/PcElectricFenceApplication.java
@@ -0,0 +1,13 @@
+package com.lyc.pcelectricfence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcElectricFenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PcElectricFenceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lyc/pcelectricfence/PcElectricFenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyc.pcelectricfence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcElectricFenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
