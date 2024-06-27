1f0cb9ac26a239a5e5b913a1436c5f09791d7260
chenqi <chenqi92104@icloud.com>
2024-06-27 09:54:32 +0800

    😀 fetch : 初始化配置丢入nettyServer中处理

--- src/main/java/com/lyc/pcelectricfence/netty/NettyServer.java
+++ src/main/java/com/lyc/pcelectricfence/netty/NettyServer.java
@@ -8,6 +8,7 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.timeout.ReadTimeoutHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +16,7 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.annotation.Resource;
 import java.net.InetSocketAddress;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 类 NettyServer
@@ -28,6 +30,11 @@ public class NettyServer {
 
     @Resource
     private NettyServerProperties nettyServerProperties;
+
+    /**
+     * 心跳超时时间
+     */
+    private static final Integer READ_TIMEOUT_SECONDS = 3 * 60;
 
     /**
      * boss 线程组，用于服务端接受客户端的连接
@@ -53,17 +60,30 @@ public class NettyServer {
     public void start() throws InterruptedException {
         // 创建 ServerBootstrap 对象，用于 Netty Server 启动
         ServerBootstrap serverBootstrap = new ServerBootstrap();
+        // 作为分隔符的数据包,防止粘包，虽然报文说是回车分隔，实际上没有，所以未使用
+//        ByteBuf delimiter = Unpooled.copiedBuffer("\r\n".getBytes());
         serverBootstrap.group(bossGroup, workerGroup)
+                // 指定 Channel 为服务端 NioServerSocketChannel
                 .channel(NioServerSocketChannel.class)
+                // 端口
                 .localAddress(new InetSocketAddress(nettyServerProperties.getPort()))
+                // 服务端接收队列的大小
                 .option(ChannelOption.SO_BACKLOG, 1024)
+                // TCP Keepalive 机制，实现 TCP 层级的心跳保活功能
                 .childOption(ChannelOption.SO_KEEPALIVE, true)
+                // 允许较小的数据包的发送，降低延迟
                 .childOption(ChannelOption.TCP_NODELAY, true)
                 .childHandler(new ChannelInitializer<SocketChannel>() {
                     @Override
                     protected void initChannel(SocketChannel ch) {
+                        // 空闲检测
+                        ch.pipeline().addLast(new ReadTimeoutHandler(READ_TIMEOUT_SECONDS, TimeUnit.SECONDS));
+//                        ch.pipeline().addLast(new DelimiterBasedFrameDecoder(16 * 1024, false, delimiter));
+                        // 解码器，因为客户端发送的是字符串所以直接用String即可，如果是其他的需要对应的解码
                         ch.pipeline().addLast(new StringDecoder());
+                        // 跟编码器同理
                         ch.pipeline().addLast(new StringEncoder());
+                        // 处理收到报文的方法
                         ch.pipeline().addLast(protocolHandler);
                     }
                 });
